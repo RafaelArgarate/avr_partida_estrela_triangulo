f9cbf6b700766b23d4e633b71e8b79312fe94a2d
RafaelArgarate <rafaelargarate@gmail.com>
2025-03-19 08:32:54 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+void main(void){
+    //Config. p/ entrada
+    DDRB&= ~(1<<1);
+    DDRB&= ~(1<<2);
+
+    //Pull up
+    PORTB|= (1<<1);
+    PORTB|= (1<<2);
+
+    //Config. p/ saída
+    DDRB|=(1<<3);
+    DDRB|=(1<<4);
+    DDRB|=(1<<5);
+
+    //Desliga contatoras
+    PORTB&= ~(1<<3);
+    PORTB&= ~(1<<4);
+    PORTB&= ~(1<<5);
+
+
+    while (1){
+        if(!(PINB & (1<<PINB2))){
+            PORTB|= (1<<3);
+            PORTB|= (1<<4);
+            _delay_ms(5000);
+            PORTB|= (1<<5);
+            PORTB&= ~(1<<4);
+        }else if(!(PINB & (1<<PINB1))){
+            PORTB&= ~(1<<3);
+            PORTB&= ~(1<<4);
+            PORTB&= ~(1<<5);
+        }
+    }
+    
+
+}
